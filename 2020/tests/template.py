51731464d47afdafd7d1720c15a76b5f7db63c49
Chris Dinneen <chris.dinneen@gmail.com>
2020-11-25 20:29:39 +0800

    Add tests template

---
+++ 2020/tests/template.py
@@ -0,0 +1,9 @@
+from dayXX import function1, function2
+
+
+class TestDayXX:
+    def test_description1(self):
+        expected = 
+        inputvalue = 
+        result = function1(inputvalue)
+        assert expected == result
