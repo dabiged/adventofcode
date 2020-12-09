2c2c70961492c2ceab858567c708b60c4f5914c6
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-09 12:28:47 +0800

    Add Template for code

---
+++ 2020/template.py
@@ -0,0 +1,21 @@
+"""
+AOC day XX 2018
+"""
+from lib.filehelper import file_to_str_array
+# pylint: disable=missing-module-docstring
+
+def dayXX_01():
+    """Run part 1 of Day XX's code"""
+    path = "./input/XX/input.txt"
+    result=""
+    print(f'XX01: {result}')
+
+def dayXX_02():
+    """Run part 2 of Day XX's code"""
+    path = "./input/XX/input.txt"
+    result=""
+    print(f'XX01: {result}')
+
+if __name__ == "__main__":
+    dayXX_01()
+    #dayXX_02()
