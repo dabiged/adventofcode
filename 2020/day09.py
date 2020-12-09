eda21387cf9abe5032320cf6d3f6d377c2552126
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-09 12:30:03 +0800

    Add skeleton day09

---
+++ 2020/day09.py
@@ -0,0 +1,21 @@
+"""
+AOC day 09 2018
+"""
+from lib.filehelper import file_to_str_array
+# pylint: disable=missing-module-docstring
+
+def day09_01():
+    """Run part 1 of Day 09's code"""
+    path = "./input/09/input.txt"
+    result=""
+    print(f'0901: {result}')
+
+def day09_02():
+    """Run part 2 of Day 09's code"""
+    path = "./input/09/input.txt"
+    result=""
+    print(f'0901: {result}')
+
+if __name__ == "__main__":
+    day09_01()
+    #day09_02()
---
+++ 2020/tests/test_day09.py
@@ -0,0 +1,9 @@
+from day09 import function1, function2
+
+
+class TestDay09:
+    def test_description1(self):
+        expected = 
+        inputvalue = 
+        result = function1(inputvalue)
+        assert expected == result
