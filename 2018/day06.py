582bc304f63dce04b9b2de618e366774f668e98c
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-11 21:58:09 +0800

    2018 day06 parts 1 and 2. Note: tests broken

--- 2018/day06.py
+++ 2018/day06.py
@@ -1,29 +1,43 @@
 """
 AOC day 06 2018
 """
+from collections import defaultdict
 from lib.filehelper import file_to_str_array
 # pylint: disable=missing-module-docstring
+
+def man_dist(x1,y1, x2,y2):
+    """Calculate the manhattan distance between two points"""
+    return abs(x1-x2)+abs(y1-y2)
 
 class Board:
     """
     A checker board like playing area of squares containing sources.
-    Each source is allocated an increasing letter and its location on the board is a capital.
-
+    Each source is allocated a letter pair and its location on the board is a capital.
     """
-    def __init__(self,listofsource, halo=2):
-        self.alphabet = {num:char for num,char in enumerate("abcdefghijklmnopqrstuvwxyz")}
+    def __init__(self,listofsource):
+        """ build a list of lists to represent the playing area.
+        halo specifies the number of blanks to the right and bottom
+        outside of input area."""
+        self.blankchar='..'
+        # Build a dict that maps source num to a alpha-char
+        self.alphabet = {num:char.upper() for num,char in enumerate([a+b for a in "abcdefghijklmnopqrstuvwxyz" for b in "abcdefghijklmnopqrstuvwxyz"])}
+        # A dict to contain source positions.
         self.sources={}
         sourcenum=0
         for source in listofsource:
-            self.sources[self.alphabet[sourcenum]]=tuple([int(i) for i in source.split(", ")])
+            self.sources[self.alphabet[sourcenum]]=tuple([int(i) for i in reversed(source.split(", "))])
             sourcenum+=1
+        # The gridded board.
         self.board=[]
-        for row in range(0,self.source_row_max()+halo):
-            thisrow = ["." for col in range(0,self.source_col_max()+halo)]
+        #populate the board with empty values.
+        self.board_size=max(self.source_row_max(),self.source_col_max())+1
+        for row in range(0,self.board_size):
+            thisrow = [self.blankchar for col in range(0,self.board_size)]
             self.board.append(thisrow)
+        # Read source locations and place them on the board
         for key, location in self.sources.items():
             row, col = location
-            self.board[col][row] = key.upper()
+            self.board[row][col] = key.upper()
 
     def __repr__(self):
         """
@@ -35,6 +49,84 @@ class Board:
         for thisrow in self.board:
             gridplot+="".join(thisrow)+"\n"
         return gridplot
+
+    def calc_influence(self):
+        '''Calculate the area that each source influences.
+        influence is determinied  by which source is closest.
+        '''
+        # for each location on the board
+        for row in range(self.board_size):
+            for col in range(self.board_size):
+                # look at each source and work out which is closest.
+                sourcedist={}
+                for sourcename, sourceloc in self.sources.items():
+                    sourcerow, sourcecol = sourceloc
+                    sourcedist[sourcename]=man_dist(row,col,sourcerow,sourcecol)
+                # make a list of all sources that are the shortest distance away.
+                closestsources=[k for k, v in sourcedist.items() if v==min(sourcedist.values())]
+                if (row,col) in self.sources.values():
+                    # location is a source.
+                    pass
+                elif len(closestsources) > 1:
+                    # location tied for multiple sources
+                    self.board[row][col]=self.blankchar
+                elif len(closestsources) == 1:
+                    # Update board with closest source.
+                    self.board[row][col] = closestsources[0].lower()
+                else:
+                    print("Something went wrong")
+
+    def create_area(self,dist=10000):
+        ''' Create and return the size of an area where all sources are
+        within dist units, measured by manhattan distance'''
+        size_area=0
+        for row in range(self.board_size):
+            for col in range(self.board_size):
+                # look at each source and work out which is closest.
+                sourcedist={}
+                for sourcename, sourceloc in self.sources.items():
+                    sourcerow, sourcecol = sourceloc
+                    sourcedist[sourcename]=man_dist(row,col,sourcerow,sourcecol)
+                # make a list of all sources that are the shortest distance away.
+                totaldist=0
+                for i in sourcedist.values():
+                    totaldist+=i
+                if totaldist<dist:
+                    self.board[row][col]='#'
+                    size_area+=1
+        return size_area
+
+
+    def count_highest_finite(self):
+        """ Return the size of the source whose influenced area is the largest
+        finite value and its name."""
+        # Create a list of all lower case infinite source.
+        # These will be excluded from all sources below.
+        # inlcude the period as these will be dropped later.
+        infinite_sources=set({self.blankchar})
+        # Look for any source area on the edges of the board.
+        for char in self.board[0][:]:
+            # Top
+            infinite_sources|=set({char.lower()})
+        for char in self.board[self.board_size-1][:]:
+            # bottom
+            infinite_sources|=set({char.lower()})
+        print(infinite_sources)
+        for row in range(self.board_size):
+            for col in [0, self.board_size-1]:
+                infinite_sources|= set({self.board[row][col].lower()})
+        print("Infinite sources:", infinite_sources)
+        positions_influenced=defaultdict(int)
+        for row in range(self.board_size):
+            for col in range(self.board_size):
+                if self.board[row][col].lower() not in infinite_sources:
+                    # Calc the number of positions influenced.
+                    positions_influenced[self.board[row][col].lower()]+=1
+        # get the name of the source with the largest finite area.
+        largest_finite_source = max(positions_influenced, key=positions_influenced.get)
+        # Return its size.
+        return positions_influenced[largest_finite_source], largest_finite_source.upper()
+
 
 
     def source_col_max(self):
@@ -53,12 +145,17 @@ class Board:
 def day06_01():
     """Run part 1 of Day 06's code"""
     path = "./input/06/input.txt"
-    print(f'0601: {result}')
+    part1board = Board(file_to_str_array(path))
+    part1board.calc_influence()
+    result, source =part1board.count_highest_finite()
+    print(f'0601: The largest finite area is: {result}')
 
 def day06_02():
     """Run part 2 of Day 06's code"""
     path = "./input/06/input.txt"
-    print(f'0602: {result}')
+    part1board = Board(file_to_str_array(path))
+    result=part1board.create_area()
+    print(f'0602: Number of spaces within 10000 units of all sources: {result}')
 
 if __name__ == "__main__":
     day06_01()
--- 2018/tests/test_day06.py
+++ 2018/tests/test_day06.py
@@ -6,14 +6,34 @@ class TestDay06:
         testinput=["350, 353", "238, 298"]
         testboard=Board(testinput)
         #assert testboard.sources == [(350,353),(238,298)]
-        assert testboard.source_col_max() == 350
-        assert testboard.source_col_min() == 238
-        assert testboard.source_row_max() == 353
-        assert testboard.source_row_min() == 298
+        assert testboard.source_row_max() == 350
+        assert testboard.source_row_min() == 238
+        assert testboard.source_col_max() == 353
+        assert testboard.source_col_min() == 298
+        assert testboard.board_size == 354
 
     def test_display_sources(self):
+        expected='..........\n.A........\n..........\n........C.\n...D......\n.....E....\n.B........\n..........\n..........\n........F.\n'
         testinput=["1, 1", "1, 6", "8, 3","3, 4","5, 5","8, 9"]
         testboard=Board(testinput)
         print(testboard)
-        assert True == False
+        print(expected)
+        assert expected == str(testboard)
 
+    def test_calc_influence(self):
+        expected='aaaaa.cccc\naAaaa.cccc\naaaddecccc\naadddeccCc\n..dDdeeccc\nbb.deEeecc\nbBb.eeee..\nbbb.eeefff\nbbb.eeffff\nbbb.ffffFf\n'
+        testinput=["1, 1", "1, 6", "8, 3","3, 4","5, 5","8, 9"]
+        testboard=Board(testinput)
+        print(testboard)
+        testboard.calc_influence()
+        print(testboard)
+        print(expected)
+        assert expected == str(testboard)
+
+    def test_compute_largest_finite(self):
+        expected = (17, 'E')
+        testinput=["1, 1", "1, 6", "8, 3","3, 4","5, 5","8, 9"]
+        testboard=Board(testinput)
+        testboard.calc_influence()
+        result=testboard.count_highest_finite()
+        assert expected == result
