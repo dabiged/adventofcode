2daad35f303dc9c8ff33628d7456af2e6c24c117
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-11 14:04:24 +0800

    2020 day11

---
+++ 2020/day11.py
@@ -0,0 +1,109 @@
+"""
+AOC day 11 2018
+"""
+from lib.filehelper import file_to_str_array
+# pylint: disable=missing-module-docstring
+
+class BoardingLounge:
+    """
+    A boarding lounge containing seats.
+    L == Vacant
+    # == Occupied
+    . == floor
+
+    """
+
+    def __init__(self,strarray):
+        self.grid=[]
+        self.numneighbours=[]
+        for row in strarray:
+            self.grid.append(list(row))
+            self.numneighbours.append(list(row))
+        self.numrows=len(self.grid)
+        self.numcols=len(self.grid[0])
+
+    def __repr__(self):
+        """
+        A helper method that allows us to run
+
+        to return a human readable representation of the run
+        """
+        gridplot=""
+        for thisrow in self.grid:
+            gridplot+="".join(thisrow)+"\n"
+        return gridplot
+    def occupied(self,row,col):
+        """ Return a bool is seat is occupied or not"""
+        if self.grid[row][col] == '#':
+            return True
+        return False
+
+    def get_neighbours(self,row,col):
+
+        if self.grid[row][col] == ".":
+            return 0
+        numneighbours=0
+        for deltarow in [-1, 0, 1]:
+            for deltacol in [-1, 0, 1]:
+                if row+deltarow <0 or row+deltarow>self.numrows-1:
+                    pass
+                elif col+deltacol <0 or col+deltacol >self.numcols-1:
+                    pass
+                elif deltarow == 0 and deltacol == 0:
+                    pass
+                elif self.occupied(row+deltarow, col+deltacol):
+                    numneighbours+=1
+        return numneighbours
+
+    def calc_neighbours(self):
+        for row in range(0,self.numrows):
+            for col in range(0,self.numcols):
+                self.numneighbours[row][col]=self.get_neighbours(row,col)
+
+    def update(self):
+        self.calc_neighbours()
+        for row in range(0,self.numrows):
+            for col in range(0,self.numcols):
+                if self.grid[row][col] == "L" \
+                and self.numneighbours[row][col] == 0:
+                    self.grid[row][col] = '#'
+                elif self.grid[row][col] == "#" \
+                and self.numneighbours[row][col] >= 4:
+                    self.grid[row][col]='L'
+
+    def run(self):
+        old_state=''
+        count=0
+        while str(self) != old_state:
+            old_state=self.__repr__()
+            self.update()
+            count+=1
+        return count
+
+    def count_occupied(self):
+        numoccupied=0
+        for row in range(0,self.numrows):
+            for col in range(0,self.numcols):
+                if self.grid[row][col]=='#':
+                    numoccupied+=1
+        return numoccupied
+
+
+
+def day11_01():
+    """Run part 1 of Day 11's code"""
+    path = "./input/11/input.txt"
+    part11lounge=BoardingLounge(file_to_str_array(path))
+    part11lounge.run()
+    result=part11lounge.count_occupied()
+    print(f'1101: {result}')
+
+def day11_02():
+    """Run part 2 of Day 11's code"""
+    path = "./input/11/input.txt"
+    result=""
+    print(f'1101: {result}')
+
+if __name__ == "__main__":
+    day11_01()
+    #day11_02()
---
+++ 2020/tests/test_day11.py
@@ -0,0 +1,67 @@
+from day11 import BoardingLounge
+from lib.filehelper import file_to_str_array
+
+
+class TestDay11:
+    def test_init_print(self):
+        testlounge = BoardingLounge(file_to_str_array('tests/day11_testdata.txt'))
+        expected='L.LL.LL.LL\nLLLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLLL\nL.LLLLLL.L\nL.LLLLL.LL\n'   
+        result = str(testlounge)
+        assert expected == result
+        assert testlounge.numrows == 10
+        assert testlounge.numrows == 10
+
+
+    def test_occupied(self):
+        expected=False
+        testlounge = BoardingLounge(file_to_str_array('tests/day11_testdata.txt'))
+        result =testlounge.occupied(0,0)
+        assert expected == result
+
+    def test_get_neighbors(self):
+        expected=0
+        testlounge = BoardingLounge(file_to_str_array('tests/day11_testdata.txt'))
+        result =testlounge.get_neighbours(0,0)
+        assert expected == result
+
+    def test_calc_neighbors(self):
+        expected=0
+        testlounge = BoardingLounge(file_to_str_array('tests/day11_testdata.txt'))
+        testlounge.calc_neighbours()
+        result = testlounge.numneighbours[0][0] 
+        assert expected == result
+
+    def test_update(self):
+        expected='#.##.##.##\n#######.##\n#.#.#..#..\n####.##.##\n#.##.##.##\n#.#####.##\n..#.#.....\n##########\n#.######.#\n#.#####.##\n'
+        testlounge = BoardingLounge(file_to_str_array('tests/day11_testdata.txt'))
+        testlounge.update()
+        result= str(testlounge)
+        assert result == expected
+
+    def test_two_updates(self):
+        expected='#.LL.L#.##\n#LLLLLL.L#\nL.L.L..L..\n#LLL.LL.L#\n#.LL.LL.LL\n#.LLLL#.##\n..L.L.....\n#LLLLLLLL#\n#.LLLLLL.L\n#.#LLLL.##\n'        
+        testlounge = BoardingLounge(file_to_str_array('tests/day11_testdata.txt'))
+        testlounge.update()
+        testlounge.update()
+        result= str(testlounge)
+        assert result == expected
+
+    def test_ten_updates(self):
+        expected='#.#L.L#.##\n#LLL#LL.L#\nL.#.L..#..\n#L##.##.L#\n#.#L.LL.LL\n#.#L#L#.##\n..L.L.....\n#L#L##L#L#\n#.LLLLLL.L\n#.#L#L#.##\n'
+        testlounge = BoardingLounge(file_to_str_array('tests/day11_testdata.txt'))
+        for _ in range(10):
+            testlounge.update()
+        result= str(testlounge)
+        assert result == expected
+
+    def test_n_updates(self):
+        expected='#.#L.L#.##\n#LLL#LL.L#\nL.#.L..#..\n#L##.##.L#\n#.#L.LL.LL\n#.#L#L#.##\n..L.L.....\n#L#L##L#L#\n#.LLLLLL.L\n#.#L#L#.##\n'
+        testlounge = BoardingLounge(file_to_str_array('tests/day11_testdata.txt'))
+        old_state=''
+        count=0
+        while str(testlounge) != old_state:
+            old_state=str(testlounge)
+            testlounge.update()
+            count+=1
+        result=str(testlounge)
+        assert result == expected
