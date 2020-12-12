dea843ef4b36fb1a739f19abb80f37a9ba2d52b5
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-12 12:40:11 +0800

    Add skeleton day12

---
+++ 2020/day12.py
@@ -0,0 +1,21 @@
+"""
+AOC day 12 2018
+"""
+from lib.filehelper import file_to_str_array
+# pylint: disable=missing-module-docstring
+
+def day12_01():
+    """Run part 1 of Day 12's code"""
+    path = "./input/12/input.txt"
+    result=""
+    print(f'1201: {result}')
+
+def day12_02():
+    """Run part 2 of Day 12's code"""
+    path = "./input/12/input.txt"
+    result=""
+    print(f'1201: {result}')
+
+if __name__ == "__main__":
+    day12_01()
+    #day12_02()
---
+++ 2020/tests/test_day12.py
@@ -0,0 +1,9 @@
+from day12 import function1, function2
+
+
+class TestDay12:
+    def test_description1(self):
+        expected = 
+        inputvalue = 
+        result = function1(inputvalue)
+        assert expected == result
