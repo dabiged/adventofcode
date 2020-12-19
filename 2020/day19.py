05ef3109f993e9eccbb81fb7ffa6c3ea5fdc7cab
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-19 14:15:54 +0800

    Add start of day 19 2020

---
+++ 2020/day19.py
@@ -0,0 +1,77 @@
+"""
+AOC day 19 2018
+"""
+from lib.filehelper import file_to_str_array
+# pylint: disable=missing-module-docstring
+
+class RegExpComputer:
+    def __init__(self, inputdata):
+        ''' Read data from file and store internally:
+        self.rules:  a dictionary  of rules, with:
+            key = rule number
+            value = list of str in the rule
+        self.messages: a list of strs containing messages '''
+
+        self.rules=[]
+        self.messages=[]
+
+        for linenum, line in enumerate(inputdata):
+            if ':' in line:
+                rulenum, rule = line.split(': ')[0], line.split(': ')[1].split('|')
+                self.rules.append([ rule.strip().replace('"','') for rule in rule ])
+            elif 'a' in line or 'b' in line:
+                self.messages.append(line.strip())
+            elif line == '':
+                pass
+            else:
+                print('Unknown input found on line:',linenum)
+                print('Input looked like \n',line)
+        print(self.rules)
+
+    def check_one_message(self,message):
+        '''
+        Check one message against all rules to see if it is valid.d
+        '''
+        # find all single chars and replace references to them with their values.
+        for rule in self.rules:
+            if rule.isalpha():
+                print(rule)
+
+        ##  allowed_msgs=[]
+        ##  for rule in self.rules:
+
+        ##  for rule in listofrules:
+        ##      look at all rules:
+        ##          if any item in any rule is a single character, 
+        ##              replace that number with the character in that rule:
+
+        ##      # 0: 4 1 5
+        ##      # 1: 2 3 | 3 2
+        ##      # 2: 4 4 | 5 5
+        ##      # 3: 4 5 | 5 4
+        ##      # 4: "a"
+        ##      # 5: "b"
+
+
+        ##      # 0: a 1 b
+        ##      # 1: 2 3 | 3 2
+        ##      # 2: a a | b b
+        ##      # 3: a b | b a
+
+        ##          
+
+def day19_01():
+    """Run part 1 of Day 19's code"""
+    path = "./input/19/input.txt"
+    result=""
+    print(f'1901: {result}')
+
+def day19_02():
+    """Run part 2 of Day 19's code"""
+    path = "./input/19/input.txt"
+    result=""
+    print(f'1902: {result}')
+
+if __name__ == "__main__":
+    day19_01()
+    #day19_02()
---
+++ 2020/tests/test_day19.py
@@ -0,0 +1,16 @@
+from day19 import RegExpComputer
+from lib.filehelper import file_to_str_array
+
+
+class TestDay19:
+    def test_read_data(self):
+        testdata='tests/day19_testdata.txt'
+        mycomp = RegExpComputer(file_to_str_array(testdata))
+        assert ['4 1 5'] in mycomp.rules
+        assert 'aaaabbb' in mycomp.messages
+
+    def test_test(self):
+        testdata='tests/day19_testdata.txt'
+        mycomp = RegExpComputer(file_to_str_array(testdata))
+        mycomp.check_one_message('blablabla')
+        assert True == False
