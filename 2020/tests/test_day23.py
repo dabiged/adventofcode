8ed9143fce0f1629f5c7f33df26450a5ec7f79b7
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-29 14:40:35 +0800

    Remove deliberately incorrect assertions

--- 2020/tests/test_day23.py
+++ 2020/tests/test_day23.py
@@ -4,11 +4,9 @@ from day23 import CrabCups
 class TestDay23:
     def test_init(self):
         mygame = CrabCups('389125467')
-        assert True == False
 
     def test_pickup(self):
         mygame = CrabCups('389125467')
         hand=mygame.pickup(3)
         print(hand)
-        assert True == False
 
