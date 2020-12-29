52a3b773a477284c0701e274edcc5c08b53f25f2
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-29 14:39:22 +0800

    2020 day 19 part2

--- 2020/day19.py
+++ 2020/day19.py
@@ -1,7 +1,7 @@
 """
 AOC day 19 2018
 """
-from itertools import product
+import re
 from lib.filehelper import file_to_str_array
 # pylint: disable=missing-module-docstring
 
@@ -10,80 +10,226 @@ class RegExpComputer:
         ''' Read data from file and store internally:
         self.rules:  a dictionary  of rules, with:
             key = rule number
-            value = list of str in the rule
-        self.messages: a list of strs containing messages '''
+            value = if tuple => tuple of rules
+                    if list of tuples => match either
+                    if str match literal.
+        self.messages: a list of strs containing messages 
 
+        rule:                         0:4 1 5 : tuple
+                                     /|\
+                                    / | \
+                                   /  |  \
+                                  a   1   b
+                                      |
+                               -------------------
+                               /                  \
+1: 2 3 | 3 2: list of tuple (2,3)                (3,2)
+                             /  \                  /  \
+                            /    \                /    \
+                           /      \              /      \
+2: 4 4 | 5 5             2         \            /        2
+3: 4 5 | 5 4            /\          3          3         /\
+                       /  \         /\        /\        /  \
+                      /    \       /  \      /  \      /    \
+4: a str             4 4   5 5  4 5  5 4    4 5 5 4  4 4    5 5
+5: b str             | |   | |  | |  | |    | | | |  | |    | |
+                     a a   b b  a b  b a    a b b a  a a    b b
+        '''
         self.rules={}
         self.messages=[]
 
         for linenum, line in enumerate(inputdata):
             if ':' in line:
-                self.rules[int(line.split(': ')[0])] = line.split(': ')[1].strip().replace('"','') 
+                key ,rule =int(line.split(': ')[0]), line.split(': ')[1]
+                # Rule section
+                if '|' in line:
+                    # An or rule
+                    or_rule=[]
+                    for subrule in rule.split('|'):
+                        or_rule.append(tuple([int(i) for i in subrule.strip().split()]))
+                    self.rules[key]=or_rule
+                elif '"' in line:
+                    # A single Char
+                    self.rules[key]=str(rule.replace('"',''))
+                else:
+                    # A list of rules e.g.  4 1 5
+                    self.rules[key]=tuple([int(i) for i in rule.strip().split()])
             elif 'a' in line or 'b' in line:
+                # Message section
                 self.messages.append(line.strip())
             elif line == '':
+                # The blank line separating rules and messages
                 pass
             else:
                 print('Unknown input found on line:',linenum)
                 print('Input looked like \n',line)
-        print(self.rules)
 
-
-    def check_one_message(self,message):
+    def build_regexp(self,rule=0):
+        '''Recursively build a regexp according to rule.
+        If the rule contains a str, this is a single char, so return it.
         '''
-        Check one message against all rules to see if it is valid.d
+        if type(self.rules[rule]) is str:
+            return self.rules[rule]
+
+        if type(self.rules[rule]) is list:
+            options=[]
+            for subrule in self.rules[rule]:
+                option=''
+                for part in subrule:
+                    option+=self.build_regexp(rule=part)
+                options.append(option)
+            return '(' + '|'.join(options)+')'
+        if type(self.rules[rule]) is tuple:
+            options=[]
+            for subrule in self.rules[rule]:
+                option=''
+                option+=self.build_regexp(rule=subrule)
+                options.append(option)
+            return '(' + ''.join(options)+')'
+
+    def match(self):
         '''
-        # find all single chars and replace references to them with their values.
-        thisstep_alpha={}
-        for rulenum, rule in self.rules.items():
-            if all([ char == '|' or char == ' ' or char.isalpha() for char in rule]):
-                thisstep_alpha[rulenum] = rule
-        print(thisstep_alpha)
-        for alphanum ,alpha in thisstep_alpha.items():
-            print("Running: ", alpha)
-            for rulenum, rule in self.rules.items():
-                if str(alphanum) in rule :
-                    self.rules[rulenum] = rule.replace(str(alphanum),alpha)
-            del self.rules[alphanum]
-        print(self.rules)
+        count the number of matches.
+        '''
+        countmatch=0
+        regexp=re.compile('^'+self.build_regexp()+'$')
+        for message in self.messages:
+            if regexp.match(message.rstrip()):
+                countmatch+=1
+        return countmatch
 
+class RegExpComputerRecursive:
+    '''Convert a Normal Choamsky form rule list into a regexp and cound the
+    number of matching messages in a list.
 
+    Allows for certain rules to be recursive'''
+    def __init__(self, inputdata):
+        ''' Read data from file and store internally
+        self.rules:  a dictionary  of rules, with:
+            key = rule number
+            value = if tuple => tuple of rules
+                    if list of tuples => match either
+                    if str match literal.
+        self.messages: a list of strs containing messages 
 
-        ##  allowed_msgs=[]
-        ##  for rule in self.rules:
+                rule:                         0:4 1 5 : tuple
+                                             /|\
+                                            / | \
+                                           /  |  \
+                                          a   1   b
+                                              |
+                                       -------------------
+                                       /                  \
+        1: 2 3 | 3 2: list of tuple (2,3)                (3,2)
+                                     /  \                  /  \
+                                    /    \                /    \
+                                   /      \              /      \
+        2: 4 4 | 5 5             2         \            /        2
+        3: 4 5 | 5 4            /\          3          3         /\
+                               /  \         /\        /\        /  \
+                              /    \       /  \      /  \      /    \
+        4: a str             4 4   5 5  4 5  5 4    4 5 5 4  4 4    5 5
+        5: b str             | |   | |  | |  | |    | | | |  | |    | |
+                             a a   b b  a b  b a    a b b a  a a    b b
+                '''
+        self.rules={}
+        self.messages=[]
 
-        ##  for rule in listofrules:
-        ##      look at all rules:
-        ##          if any item in any rule is a single character, 
-        ##              replace that number with the character in that rule:
+        for linenum, line in enumerate(inputdata):
+            if ':' in line:
+                key ,rule =int(line.split(': ')[0]), line.split(': ')[1]
+                # Rule section
+                if '|' in line:
+                    # An or rule
+                    or_rule=[]
+                    for subrule in rule.split('|'):
+                        or_rule.append(tuple([int(i) for i in subrule.strip().split()]))
+                    self.rules[key]=or_rule
+                elif '"' in line:
+                    # A single Char
+                    self.rules[key]=str(rule.replace('"',''))
+                else:
+                    # A list of rules e.g.  4 1 5
+                    self.rules[key]=tuple([int(i) for i in rule.strip().split()])
+            elif 'a' in line or 'b' in line:
+                # Message section
+                self.messages.append(line.strip())
+            elif line == '':
+                # The blank line separating rules and messages
+                pass
+            else:
+                print('Unknown input found on line:',linenum)
+                print('Input looked like \n',line)
 
-        ##      # 0: 4 1 5
-        ##      # 1: 2 3 | 3 2
-        ##      # 2: 4 4 | 5 5
-        ##      # 3: 4 5 | 5 4
-        ##      # 4: "a"
-        ##      # 5: "b"
+        self.rules[8] = [(42),(42,8)]
+        self.rules[11] = [(42,31),(42,11,31)]
 
+    def build_regexp(self,rule=0):
+        '''Recursively build a regexp according to rule.
+        If the rule contains a str, this is a single char, so return it.
+        if the rule is a list, make an or statement of each subrule.
+        if the rule is a tuple, it is just a concatenated series of rules.
+        '''
+        if rule ==8:
+            # rule 8 translates to: match one or more of rule 42.
+            # Use the regexp '+' for this
+            return '('+self.build_regexp(rule=42)+')+'
 
-        ##      # 0: a 1 b
-        ##      # 1: 2 3 | 3 2
-        ##      # 2: a a | b b
-        ##      # 3: a b | b a
+        if rule == 11:
+            # rule 11 translate to:
+            #    match ab or aabb or aaabbb or aaaabbbb ...
+            # or     a{1}b{1}|a{2}b{2}|a{3}b{3}|a{4}b{4}
+            output=''
+            for i in range(1,len(max(self.messages, key=len))//2):
+                output+=self.build_regexp(rule=42)+'{'+str(i)+'}'+self.build_regexp(rule=31)+'{'+str(i)+'}|'
+            # drop the final or char.
+            output=output[:-1]
+            return '('+output+')'
 
-        ##          
+        if type(self.rules[rule]) is str:
+            return self.rules[rule]
+
+        if type(self.rules[rule]) is list:
+            options=[]
+            for subrule in self.rules[rule]:
+                option=''
+                for part in subrule:
+                    option+=self.build_regexp(rule=part)
+                options.append(option)
+            return '(' + '|'.join(options)+')'
+        if type(self.rules[rule]) is tuple:
+            options=[]
+            for subrule in self.rules[rule]:
+                option=''
+                option+=self.build_regexp(rule=subrule)
+                options.append(option)
+            return '(' + ''.join(options)+')'
+
+    def match(self):
+        '''
+        count the number of matches.
+        '''
+        countmatch=0
+        regexp=re.compile('^'+self.build_regexp()+'$')
+        for message in self.messages:
+            if regexp.match(message.rstrip()):
+                countmatch+=1
+        return countmatch
 
 def day19_01():
     """Run part 1 of Day 19's code"""
     path = "./input/19/input.txt"
-    result=""
-    print(f'1901: {result}')
+    mycomp = RegExpComputer(file_to_str_array(path))
+    result=mycomp.match()
+    print(f'1901: number of messages that match: {result}')
 
 def day19_02():
     """Run part 2 of Day 19's code"""
     path = "./input/19/input.txt"
-    result=""
-    print(f'1902: {result}')
+    mycomp = RegExpComputerRecursive(file_to_str_array(path))
+    result=mycomp.match()
+    print(f'1902: number of messages that match: {result}')
 
 if __name__ == "__main__":
     day19_01()
-    #day19_02()
+    day19_02()
--- 2020/tests/test_day19.py
+++ 2020/tests/test_day19.py
@@ -1,18 +1,59 @@
-from day19 import RegExpComputer
+from day19 import RegExpComputer, RegExpComputerRecursive
 from lib.filehelper import file_to_str_array
 
 
-#class TestDay19:
-#    def test_read_data(self):
-#        testdata='tests/day19_testdata.txt'
-#        mycomp = RegExpComputer(file_to_str_array(testdata))
-#        assert ['4 1 5'] in mycomp.rules
-#        assert 'aaaabbb' in mycomp.messages
-#
-#    def test_test(self):
-#        testdata='tests/day19_testdata.txt'
-#        mycomp = RegExpComputer(file_to_str_array(testdata))
-#        mycomp.check_one_message('blablabla')
-#        mycomp.check_one_message('blablabla')
-#        assert True == False
-pass
+class TestDay19:
+    def test_read_data(self):
+        testdata='tests/day19_testdata.txt'
+        mycomp = RegExpComputer(file_to_str_array(testdata))
+        assert (4, 1, 5) == mycomp.rules[0]
+        assert 'aaaabbb' in mycomp.messages
+
+    def test_build_regexp(self):
+        testdata='tests/day19_testdata.txt'
+        mycomp = RegExpComputer(file_to_str_array(testdata))
+        # strs
+        assert mycomp.build_regexp(rule=4)== 'a'
+        assert mycomp.build_regexp(rule=5)== 'b'
+        # lists
+        assert '(aa|bb)' == mycomp.build_regexp(rule=2)
+        # tuples
+        assert '(a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b)' == mycomp.build_regexp()
+
+    def test_match(self):
+        testdata='tests/day19_testdata.txt'
+        mycomp = RegExpComputer(file_to_str_array(testdata))
+        result=mycomp.match()
+        assert result == 2
+
+    def test_read_data_recursive(self):
+        testdata='tests/day19_testdata2.txt'
+        mycomp = RegExpComputerRecursive(file_to_str_array(testdata))
+        assert (8, 11) == mycomp.rules[0]
+        assert 'bbabbbbaabaabba' in mycomp.messages
+
+    def test_build_regexp_recursive(self):
+        testdata='tests/day19_testdata2.txt'
+        mycomp = RegExpComputerRecursive(file_to_str_array(testdata))
+        # strs
+        assert mycomp.build_regexp(rule=14)== 'b'
+        assert mycomp.build_regexp(rule=1)== 'a'
+        # lists
+        assert '(aa|ab)' == mycomp.build_regexp(rule=25)
+        # tuples
+        assert '(aa)' == mycomp.build_regexp(rule=4)
+        # rule 8. Replace rule 42 with a single char.
+        mycomp.rules[42] = 'x'
+        # Rule 8 is just 1 or more of rule 42.
+        assert '(x)+' ==  mycomp.build_regexp(rule=8)
+        # rule 11. replace rule 31 with a single char
+        mycomp.rules[31] = 'y'
+        # Rule 11 is of the form xy|xxyy|xxxyyy|xxxxyyyy aka x{1}y{1}|x{2}y{2}...
+        assert 'x{1}y{1}|x{2}y{2}' in  mycomp.build_regexp(rule=11)
+
+    def test_match(self):
+        testdata='tests/day19_testdata2.txt'
+        mycomp = RegExpComputerRecursive(file_to_str_array(testdata))
+        result=mycomp.match()
+        assert result == 12
+
