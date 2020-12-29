30f835aceefb969962d9c86c047395facfe877ef
Chris Dinneen <chris.dinneen@gmail.com>
2020-12-29 22:30:29 +0800

    2020 day 24 part1

--- 2020/day24.py
+++ 2020/day24.py
@@ -6,14 +6,9 @@ from lib.filehelper import file_to_str_array
 # pylint: disable=missing-module-docstring
 
 class HexBoard:
-    '''      
-            /  \
-           |    |  col +1
-            \  /
-       row+1     row+1,col+1
-
-
-
+    '''Create a hexagon based board.
+    Offset odd rows by 1/2 space.
+    left and right sides of the hexagons are vertical.
     '''
     def __init__(self, instr):
         '''Read in instructions and initialise the hex board'''
@@ -22,10 +17,11 @@ class HexBoard:
         with open(instr) as f:
             instructions = f.read()
         for instr in instructions.split('\n'):
+            if instr == '':
+                # Skip blank lines
+                continue
             # instr is a str
             i=0
-            if i == 0:
-                print(instr)
             thisinstr=[]
             while i < len(instr):
                 if instr[i] in ('n','s'):
@@ -40,43 +36,119 @@ class HexBoard:
 
 
 
+
+    def west(self,rcloc):
+        row,col=rcloc
+        return (row,col-1)
+
+    def east(self,rcloc):
+        row,col=rcloc
+        return (row,col+1)
+
+    def southwest(self,rcloc):
+        row,col=rcloc
+        if row % 2 == 1:
+            return (row-1,col-1)
+        return (row-1,col)
+
+    def southeast(self,rcloc):
+        row,col=rcloc
+        if row % 2 == 1:
+            return (row-1,col)
+        return (row-1,col+1)
+
+    def northeast(self,rcloc):
+        row,col=rcloc
+        if row % 2 == 1:
+            return (row+1,col)
+        return (row+1,col+1)
+
+    def northwest(self,rcloc):
+        row,col=rcloc
+        if row % 2 == 1:
+            return (row+1,col-1)
+        return (row+1,col)
+
+    def move(self,instr,loc):
+        '''Given a single movement, change the location'''
+        if   instr == 'e':
+            return self.east(loc)
+        elif instr == 'w':
+            return self.west(loc)
+        elif instr == 'nw':
+            return self.northwest(loc)
+        elif instr == 'ne':
+            return self.northeast(loc)
+        elif instr == 'sw':
+            return self.southwest(loc)
+        elif instr == 'se':
+            return self.southeast(loc)
+        raise ValueError(f'Unknown Instruction: {instr}')
+
+    def locate_tile(self,instruction):
+        ''' Read an instruction and return the location of the tile reached in that instruction'''
+        location=(0,0)
+        for instr in instruction:
+            location=self.move(instr,location)
+        return location
+
+
+    def run_instr(self,instruction):
+        '''Read an instruction and flip the tile at the location given'''
+        location=self.locate_tile(instruction)
+        current_value=self.board.get(location,0)
+        if current_value == 0:
+            self.board[location] =1
+        elif current_value==1:
+            del self.board[location]
+
+    def setup(self):
+        ''' Run all instructions in the instruction list'''
+        for instruction in self.instructions:
+            self.run_instr(instruction)
+
+    def count_black_tiles(self):
+        blacktiles=0
+        for location,numflips in self.board.items():
+            if numflips==1:
+                blacktiles+=1
+        return blacktiles
+
+    def count_occupied(self,loc):
+        numoccupied=0
+        for thispoint in self.get_neighbours(loc):
+            if self.board.get(thispoint,0) ==1:
+                numoccupied +=1
+        return numoccupied
+
     def get_neighbours(self,rcloc):
         ''' given a tuple location rcloc, give all neighbours'''
         for loc in [self.west(rcloc),self.northwest(rcloc),self.northeast(rcloc),\
                     self.east(rcloc),self.southeast(rcloc),self.southwest(rcloc)]:
             yield loc
 
-    def west(self,rcloc):
-        row,col=rcloc
-        return (row,col-1)
-
-    def southwest(self,rcloc):
-        row,col=rcloc
-        return (row-1,col-1)
-
-    def southeast(self,rcloc):
-        row,col=rcloc
-        return (row-1,col)
-
-    def east(self,rcloc):
-        row,col=rcloc
-        return (row,col+1)
-
-    def northeast(self,rcloc):
-        row,col=rcloc
-        return (row+1,col+1)
-
-    def northwest(self,rcloc):
-        row,col=rcloc
-        return (row+1,col)
-
-
+    def step(self):
+        '''Step the tile floor 1 day'''
+        # Count the number of neighbours of each hex.
+        black_tile_num_neighbours={}
+        # Note the use of a set comprehension here so we only count each one once.
+        for thishex in {neighbourhex for centrehex in self.board\
+                    for neighbourhex in self.get_neighbours(centrehex)}:
+            black_tile_num_neighbours[thishex]=self.count_occupied(thishex)
+        print(black_tile_num_neighbours)
+        for thishex, numneighbours in black_tile_num_neighbours.items():
+            if self.board.get(thishex,0) == 1 and numneighbours in (0,2,3,4,5,6):
+                del self.board[thishex] 
+            elif self.board.get(thishex,0) == 0 and numneighbours == 2:
+                self.board[thishex]=1
 
 def day24_01():
     """Run part 1 of Day 24's code"""
     path = "./input/24/input.txt"
-    result=""
-    print(f'2401: {result}')
+    myhex = HexBoard(path)
+    myhex.setup()
+    result = myhex.count_black_tiles()
+    print(f'2401: Number of Black tiles:{result}')
 
 def day24_02():
     """Run part 2 of Day 24's code"""
--- 2020/tests/test_day24.py
+++ 2020/tests/test_day24.py
@@ -7,16 +7,38 @@ class TestDay24:
         myhex = HexBoard(inputdata)
         assert myhex.instructions[2]==['se','sw','ne','sw','sw','se','nw','w','nw','se']
 
-    def test_movearound(self):
+    def test_movearound_basic(self):
         inputdata='tests/day24_testinput.txt'
         myhex = HexBoard(inputdata)
         west=myhex.west((0,0))
         origin=myhex.east(west)
         assert origin == (0,0)
+        west=myhex.west((0,0))
         sw=myhex.southeast(west)
         se=myhex.east(sw)
         w=myhex.northeast(se)
         nw=myhex.northwest(w)
         ne=myhex.west(nw)
-        origin=myhex.southwest(ne)
+        origin=myhex.southeast(ne)
         assert origin == (0,0)
+
+    def test_locate_tile(self):
+        inputdata='tests/day24_testinput.txt'
+        myhex = HexBoard(inputdata)
+        myhex.instructions[0]=['w','se','e','ne','nw','w','se']
+        result = myhex.locate_tile(myhex.instructions[0])
+        assert result == (0,0)
+
+    def test_example1(self):
+        inputdata='tests/day24_testinput.txt'
+        myhex = HexBoard(inputdata)
+        myhex.setup()
+        myhex.step()
+        result = myhex.count_black_tiles()
+        assert result == 15
+        myhex.step()
+        result = myhex.count_black_tiles()
+        assert result == 12
+        myhex.step()
+        result = myhex.count_black_tiles()
+        assert result == 25
