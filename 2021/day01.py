6203570bed59c0a8411abf1c0d8bda9ea5c81c86
Chris Dinneen <chris.dinneen@gmail.com>
2021-12-05 09:19:34 +0800

    refactor day 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -4,10 +4,8 @@ Advent of Code Day 01 2021
 from lib.filehelper import file_to_array
 # pylint: disable=missing-module-docstring
 
-def day01_01():
+def gt_previous(inputlist):
     """Run part 1 of Day 1's code"""
-    PATH = "./input/day01.txt"
-    inputlist=file_to_array(PATH)
     count=0
     old=9999
     for i in inputlist:
@@ -15,13 +13,11 @@ def day01_01():
             count+=1
         old=i
 
-    print(f"0101: {count} Measurements larger than the previous measurement.")
+    return count
 
 
-def day01_02():
+def gt_previous_smoothed(inputlist):
     """Run part 2 of Day 1's code"""
-    PATH = "./input/day01.txt"
-    inputlist=file_to_array(PATH)
     count=0
     old=9999
     for i,_ in enumerate(inputlist):
@@ -30,8 +26,20 @@ def day01_02():
             if sumof3>old:
                 count+=1
             old=sumof3
+    return count
 
-    print(f"0102: {count} sums larger than the previous sum.")
+
+def day01_01():
+    PATH = "./input/day01.txt"
+    inputlist=file_to_array(PATH)
+    result=gt_previous(inputlist)
+    print(f"0101: {result} Measurements larger than the previous measurement.")
+
+def day01_02():
+    PATH = "./input/day01.txt"
+    inputlist=file_to_array(PATH)
+    result=gt_previous_smoothed(inputlist)
+    print(f"0102: {result} sums larger than the previous sum.")
 
 if __name__ == "__main__":
     day01_01()
--- 2021/tests/test_day01.py
+++ 2021/tests/test_day01.py
@@ -1,16 +1,18 @@
-#from day03 import day03_01,day03_02
-#from lib.filehelper import file_to_str_array
-## pylint: disable=missing-class-docstring
-## pylint: disable=missing-function-docstring
-#
-#class TestDay03:
-#    # pylint: disable=missing-module-docstring
-#    def test_01(self):
-#        result=day03_01(file_to_str_array('tests/input_day3.txt'))
-#        expected = ("01001",9)
-#        assert expected == result
-#
-#    def test_02(self):
-#        result=day03_02([199,200,208,210,200,207,240,269,260,263])
-#        expected = 5
-#        assert expected == result
+from day01 import gt_previous,gt_previous_smoothed
+from lib.filehelper import file_to_array
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+class TestDay01:
+    # pylint: disable=missing-module-docstring
+    def test_01(self):
+        testlist=file_to_array('tests/input_day01.txt')
+        result=gt_previous(testlist)
+        expected = 7
+        assert expected == result
+
+    def test_02(self):
+        testlist=file_to_array('tests/input_day01.txt')
+        result=gt_previous_smoothed(testlist)
+        expected = 5
+        assert expected == result
