f161aa83a75a64243dcc7cd4d023091f3ea3e16d
Chris Dinneen <chris.dinneen@gmail.com>
2021-12-10 13:04:12 +0800

    Update template

--- 2021/template.py
+++ 2021/template.py
@@ -4,6 +4,16 @@ AOC day XX 2018
 from lib.filehelper import file_to_str_array
 # pylint: disable=missing-module-docstring
 
+class dayXX():
+    def __init__(self,inputfile):
+        self.inputfile=inputfile
+        self.processinputfile()
+
+    def processinputfile()
+        self.input=file_to_str_array(self.inputfile)
+
+
+
 def dayXX_01():
     """Run part 1 of Day XX's code"""
     path = "./input/XX/input.txt"
