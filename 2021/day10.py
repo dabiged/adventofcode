14e957e1c29967eb2837a5b026602ce17642526e
Chris Dinneen <chris.dinneen@gmail.com>
2021-12-10 13:04:28 +0800

    WIP: Day 10 2021

---
+++ 2021/day10.py
@@ -0,0 +1,31 @@
+"""
+AOC day 10 2018
+"""
+from lib.filehelper import file_to_str_array
+# pylint: disable=missing-module-docstring
+
+class SyntaxFixer():
+    def __init__(self,inputfile):
+        self.inputfile=inputfile
+        self.processinputfile()
+
+    def processinputfile()
+        self.input=file_to_str_array(self.inputfile)
+
+
+
+def day10_01():
+    """Run part 1 of Day 10's code"""
+    path = "./input/10/input.txt"
+    result=""
+    print(f'1001: {result}')
+
+def day10_02():
+    """Run part 2 of Day 10's code"""
+    path = "./input/10/input.txt"
+    result=""
+    print(f'1002: {result}')
+
+if __name__ == "__main__":
+    day10_01()
+    #day10_02()
---
+++ 2021/tests/test_day10.py
@@ -0,0 +1,15 @@
+from day10 import *
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+class TestDay10:
+    # pylint: disable=missing-module-docstring
+    def test_01(self):
+        inputfile = "./tests/input_day10.txt"
+        test=SyntaxFixer(inputfile)
+        print(test.input)
+        assert result == 15
+
+    def test_02(self):
+        pass
+
