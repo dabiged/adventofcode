2d9f2e2019b891cc78f1fc6d6d0f452e11442842
Chris Dinneen <chris.dinneen@gmail.com>
2021-12-13 16:57:49 +0800

    202112

---
+++ 2021/day12.py
@@ -0,0 +1,122 @@
+"""
+AOC day XX 2018
+"""
+from lib.filehelper import file_to_str_array
+from collections import defaultdict, deque
+from copy import deepcopy
+# pylint: disable=missing-module-docstring
+
+class Journey():
+    def __init__(self,startlocation):
+        self.nodesvisited=startlocation
+
+    def newnode(self,node):
+        self.nodesvisited.append(node)
+
+    def currentnode(self):
+        return self.nodesvisited[-1]
+
+    def isValid(self):
+        node_visit_count=defaultdict(int)
+        for i in self.nodesvisited:
+            node_visit_count[i]+=1
+        for key,value in node_visit_count.items():
+            if key == key.lower() and value > 1:
+                return False
+        return True
+
+    def isp2Valid(self):
+        node_visit_count=defaultdict(int)
+        if self.nodesvisited.count('end') > 1:
+            return False
+        if self.nodesvisited.count('start') > 1:
+            return False
+        for i in self.nodesvisited:
+            node_visit_count[i]+=1
+        smallcaves=0
+        for key,value in node_visit_count.items():
+            if key == key.lower() and value > 1:
+                if value >2:
+                    return False
+                smallcaves+=1
+                if smallcaves > 1:
+                    return False
+
+        return True
+
+    def isFinished(self):
+        if 'end' in self.nodesvisited and 'start' in self.nodesvisited:
+            return True
+        return False
+
+    def __repr__(self):
+        return str(self.nodesvisited)
+
+class Graph():
+    def __init__(self,inputfile):
+        self.inputfile=inputfile
+        self.graph=defaultdict(list)
+        self.traverses={}
+        self.processinputfile()
+        self.validjourneys=[]
+
+    def processinputfile(self):
+        self.input=file_to_str_array(self.inputfile)
+        for line in self.input:
+            nodes=line.split('-')
+            from_node,to_node=nodes[0],nodes[1]
+            self.graph[from_node].append(to_node)
+            self.graph[to_node].append(from_node)
+
+
+    def traverse(self):
+        travel_queue=deque()
+        travel_queue.append(Journey(['start']))
+        while len(travel_queue) > 0:
+            currentjourney=travel_queue.popleft()
+            if not currentjourney.isValid():
+                pass
+            elif currentjourney.isFinished():
+                self.validjourneys.append(currentjourney)
+            else:
+                for newdest in self.graph[currentjourney.currentnode()]:
+                    thisjourn=deepcopy(currentjourney)
+                    currentnodes=thisjourn.nodesvisited
+                    currentnodes.append(newdest)
+                    travel_queue.append(Journey(currentnodes))
+
+    def retraverse(self):
+        travel_queue=deque()
+        travel_queue.append(Journey(['start']))
+        while len(travel_queue) > 0:
+            currentjourney=travel_queue.popleft()
+            if currentjourney.isFinished():
+                self.validjourneys.append(currentjourney)
+            elif not currentjourney.isp2Valid():
+                pass
+            else:
+                for newdest in self.graph[currentjourney.currentnode()]:
+                    thisjourn=deepcopy(currentjourney)
+                    currentnodes=thisjourn.nodesvisited
+                    currentnodes.append(newdest)
+                    travel_queue.append(Journey(currentnodes))
+
+def day12_01():
+    """Run part 1 of Day 12's code"""
+    inputfile='input/day12.txt'
+    myGraph=Graph(inputfile)
+    myGraph.traverse()
+    result=len(myGraph.validjourneys)
+    print(f'1201: {result}')
+
+def day12_02():
+    """Run part 2 of Day XX's code"""
+    inputfile='input/day12.txt'
+    myGraph=Graph(inputfile)
+    myGraph.retraverse()
+    result= len(myGraph.validjourneys) 
+    print(f'1202: {result}')
+
+if __name__ == "__main__":
+    day12_01()
+    day12_02()
---
+++ 2021/tests/test_day12.py
@@ -0,0 +1,40 @@
+from day12 import *
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+class TestDay12:
+    # pylint: disable=missing-module-docstring
+    def test_01(self):
+        inputfile='tests/input_day12.txt'
+        myGraph=Graph(inputfile)
+        myGraph.traverse()
+        assert len(myGraph.validjourneys) == 10
+
+    def test_02(self):
+        inputfile='tests/input_day12a.txt'
+        myGraph=Graph(inputfile)
+        myGraph.traverse()
+        assert len(myGraph.validjourneys) == 19
+
+    def test_03(self):
+        inputfile='tests/input_day12b.txt'
+        myGraph=Graph(inputfile)
+        myGraph.traverse()
+        assert len(myGraph.validjourneys) == 226
+
+    def test_04(self):
+        inputfile='tests/input_day12.txt'
+        myGraph=Graph(inputfile)
+        myGraph.retraverse()
+        assert len(myGraph.validjourneys) == 36
+    def test_05(self):
+        inputfile='tests/input_day12a.txt'
+        myGraph=Graph(inputfile)
+        myGraph.retraverse()
+        assert len(myGraph.validjourneys) == 103
+
+    def test_06(self):
+        inputfile='tests/input_day12b.txt'
+        myGraph=Graph(inputfile)
+        myGraph.retraverse()
+        assert len(myGraph.validjourneys) == 3509
