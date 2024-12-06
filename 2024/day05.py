6ed7e5637286724bb95a10ea3690a60fd21eff19
Chris Dinneen <chris.dinneen@tapeark.com>
2024-12-06 17:28:28 +0800

    202405

--- 2024/day05.py
+++ 2024/day05.py
@@ -32,10 +32,8 @@ testdata='''47|53
 61,13,29
 97,13,75,29,47'''
 
-global postrules
-global prerules
-prerules=defaultdict(set)
-postrules=defaultdict(set)
+global allrules
+allrules=defaultdict(list)
 
 def parse(ipt):
     rules=[]
@@ -51,87 +49,44 @@ def parse(ipt):
             raise ValueError
     return rules, updates
 
-def str_2_strarray(string):
-    return string.split('\n')
-
-def isValid(update,prerules,postrules):
-    for i, num in enumerate(update):
-        #print(update, num)
-        if len(postrules[num]) >0:
-            for notallowedafter in postrules[num]:
-                if notallowedafter in update[i:]:
-                    #print(notallowedafter, 'occurs after', num,' inviolation of',prerules[num])
-                    return False
-        if len(prerules[num]) >0:
-            for notallowedbefore in prerules[num]:
-                if notallowedbefore in update[:i]:
-                    #print(notallowedafter, 'occurs after', num,' inviolation of',prerules[num])
-                    return False
-
-    return True
-
-
-def part1(inputdata=str_2_strarray(testdata)):
-    rules, updates = parse(inputdata)
-    prerules=defaultdict(set)
-    postrules=defaultdict(set)
-    for rule in rules:
-        a=rule.split('|')[0]
-        b=rule.split('|')[1]
-        prerules[a].add(b)
-        postrules[b].add(a)
-    score=0
-    for update in updates:
-        update=update.split(',')
-        if isValid(update,prerules,postrules):
-            score+=int(update[len(update)//2])
-    return score
-
-
 def cmp_updates(a,b):
     # A custom sort function:
     # return 1 if a goes before b
     # return 0 if you dont care about the order
     # return -1 if b goes before a
-    if b in prerules[a]:
+    if b in allrules[a]:
         return -1
-    elif b in postrules[a]:
+    elif a in allrules[b]:
         return 1
     else:
         return 0
 
-def part2(inputdata=str_2_strarray(testdata)):
+def run(inputdata=testdata.split('\n')):
     rules, updates = parse(inputdata)
+    # build rules
     for rule in rules:
-        a=rule.split('|')[0]
-        b=rule.split('|')[1]
-        prerules[a].add(b)
-        postrules[b].add(a)
-    score=0
+        a=int(rule.split('|')[0])
+        b=int(rule.split('|')[1])
+        allrules[a].append(b)
+
+    score1,score2=0,0
     for update in updates:
-        update=update.split(',')
-        if not isValid(update,prerules,postrules):
-            print(update)
-            update.sort(key=cmp_to_key(cmp_updates))
-            print(update)
-            score+=int(update[len(update)//2])
-    return score
+        nums=list(map(int,update.split(',')))
+        # Sort the list using our custom function
+        sorted_nums=sorted(nums,key=cmp_to_key(cmp_updates))
+        if nums == sorted_nums:
+            score1+=int(nums[len(nums)//2])
+        else:
+            score2+=int(sorted_nums[len(sorted_nums)//2])
 
-
+    return score1,score2
 
 def day05_01():
     """Run part 1 of Day 05's code"""
     path = "./input/05.txt"
-    print("0501:", part1(file_to_str_array(path)))
-
-
-def day05_02():
-    """Run part 2 of Day 05's code"""
-    path = "./input/05.txt"
-    print("0502:", part2(file_to_str_array(path)))
+    a,b=run(file_to_str_array(path))
+    print("0501:", a)
+    print("0502:", b)
 
 if __name__ == "__main__":
-    #print(part1())
     day05_01()
-    print(part2())
-    day05_02()
